2c76cc70485a32f8a1b0bf94b31cf82503f01b07
George Black <georgemblack@users.noreply.github.com>
2018-03-02 14:51:29 -0600

    Sweep and write register on interval from program start

--- whisk.go
+++ whisk.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
-	logFilePath = "log.whisk"
-	port        = "8081"
+	logFilePath   = "log.whisk"
+	cleanInterval = time.Minute * 15
+	port          = "8081"
 )
 
 // Launch starts server, initializes log, register, etc.
@@ -17,14 +19,13 @@ func Launch() {
 
 	initializeLog()
 	initializeRegister()
+	initializeTimer()
 
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
 	http.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
 		createPage("./sample.md")
 	}) // temporary
-	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
-		writeRegister()
-	}) // temporary
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		address := r.URL.Path[1:]
 		if len(address) == 0 {
@@ -42,10 +43,22 @@ func Launch() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
+// initializeLog creates log file if it doesn't exist, sets output
 func initializeLog() {
 	f, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Error initializing log file: %s\n", err)
 	}
 	log.SetOutput(f)
+}
+
+// initializeTimer will sweep/write the register on given interval
+func initializeTimer() {
+	ticker := time.NewTicker(cleanInterval)
+	go func() {
+		for range ticker.C {
+			sweepRegister()
+			writeRegister()
+		}
+	}()
 }
