e692eecd6156e02f564018384b2836722352c002
George Black <georgemblack@users.noreply.github.com>
2018-03-02 16:41:34 -0600

    Catch interrupt and perform cleanup before exiting

--- cmd/whisk/main.go
+++ cmd/whisk/main.go
@@ -1,7 +1,22 @@
 package main
 
-import whisk "github.com/georgemblack/whisk"
+import (
+	"fmt"
+	"github.com/georgemblack/whisk"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Print("\nCleaning up...\n")
+		whisk.Cleanup()
+		os.Exit(0)
+	}()
+
 	whisk.Launch()
 }
