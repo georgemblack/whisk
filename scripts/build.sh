77d069cf2e6277ed86bfd1912031820aa7d4d96a
George Black <georgemblack@users.noreply.github.com>
2018-03-08 14:33:13 -0600

    Remove server start from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 sass ./resources/main.scss ./public/styles/main.css --style compressed
 sass ./resources/themes/minimal.scss ./public/styles/minimal.css --style compressed
 rm -rf ./public/styles/*.css.map ./.sass-cache/
-go run cmd/whisk/main.go
